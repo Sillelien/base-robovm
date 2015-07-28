f1f9cedf8dded1eff4bcf47508ea3e525dd8ab12
neilellis <hello@neilellis.me>
2015-07-28 18:12:38 +0100

    Initial work

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env ash
+set -ex
+apk -U add git wget openssl libstdc++
+wget "https://get.docker.com/builds/Linux/x86_64/docker-latest" -O /usr/local/bin/docker
+chmod 755 /usr/local/bin/docker
+wget "http://download.robovm.org/robovm-1.4.0.tar.gz"
+tar xvfz robovm-1.4.0.tar.gz
+mv robovm-1.4.0 /usr/local/robovm
+
